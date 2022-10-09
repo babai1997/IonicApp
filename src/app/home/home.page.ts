ba8d585046d646a1ee1b16aa2d6f16c4b1dbeb71
unknown <sudipta.pramanik1997@gmail.com>
2022-10-09 13:00:24 +0530

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
 
+import { ActionSheetController } from '@ionic/angular';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
+  result: string;
 
-  constructor() {}
+  constructor(private actionSheetCtrl: ActionSheetController) {}
 
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'home header',
+      subHeader: 'home subheader',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+        },
+        {
+          text: 'Share',
+          data: {
+            action: 'share',
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
+
+    const result = await actionSheet.onDidDismiss();
+    this.result = JSON.stringify(result, null, 2);
+  }
 }
